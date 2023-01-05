cd5f5bae2a60c0d7f5a96366061aa3974b819dd4
BobaFettyW4p <matthew.ivancic91@gmail.com>
2023-01-05 13:51:33 -0600

    restructure

--- tests/gutenbergPasswordGenerator_test.py
+++ tests/gutenbergPasswordGenerator_test.py
@@ -1,7 +1,7 @@
 import sys
-sys.path.insert(0,'./')
+sys.path.insert(0,'./app/')
 
-import string, pytest, gutenbergpy.textget, GetNovelText
+import string, pytest, gutenbergpy.textget, app.gutenbergPasswordGenerator as gutenbergPasswordGenerator
 
 
 @pytest.fixture
@@ -11,18 +11,18 @@ def sample_text():
 
 @pytest.fixture
 def sample_dictionary():
-    book = GetNovelText.get_book(890)
-    final_book = GetNovelText.clean_book(book)
+    book = gutenbergPasswordGenerator.get_book(890)
+    final_book = gutenbergPasswordGenerator.clean_book(book)
     return final_book
 
 def test_get_book(sample_text):
-    assert type(GetNovelText.get_book(890)) is bytes
+    assert type(gutenbergPasswordGenerator.get_book(890)) is bytes
 
 def test_clean_book_type(sample_text):
-    assert type(GetNovelText.clean_book(sample_text)) is list
+    assert type(gutenbergPasswordGenerator.clean_book(sample_text)) is list
 
 def test_clean_book_word_length(sample_text):
-    book = GetNovelText.clean_book(sample_text)
+    book = gutenbergPasswordGenerator.clean_book(sample_text)
     valid = True
     for page in book:
         if len(page) < 4:
@@ -31,7 +31,7 @@ def test_clean_book_word_length(sample_text):
     assert valid == True
 
 def test_capitalizaton_clean_book(sample_text):
-    book = GetNovelText.clean_book(sample_text)
+    book = gutenbergPasswordGenerator.clean_book(sample_text)
     valid = True
     for page in book:
         if page[0] in string.ascii_lowercase:
@@ -40,7 +40,7 @@ def test_capitalizaton_clean_book(sample_text):
     assert valid
 
 def test_clean_book_word_length(sample_text):
-    book = GetNovelText.clean_book(sample_text)
+    book = gutenbergPasswordGenerator.clean_book(sample_text)
     valid = True
     for word in book:
         if len(word) <= 3:
@@ -49,7 +49,7 @@ def test_clean_book_word_length(sample_text):
     assert valid
 
 def test_clean_book_uniqueness(sample_text):
-    book = GetNovelText.clean_book(sample_text)
+    book = gutenbergPasswordGenerator.clean_book(sample_text)
     valid = True
     for word in book:
         if book.count(word) != 1:
@@ -59,12 +59,12 @@ def test_clean_book_uniqueness(sample_text):
 
 def test_generate_password_candidates(sample_dictionary):
     PASSWORD_LENGTH = 20
-    candidates = GetNovelText.generate_password(sample_dictionary,PASSWORD_LENGTH)
+    candidates = gutenbergPasswordGenerator.generate_password(sample_dictionary,PASSWORD_LENGTH)
     assert len(candidates) == 5
 
 def test_generate_password_two_digits(sample_dictionary):
     PASSWORD_LENGTH = 20
-    candidates = GetNovelText.generate_password(sample_dictionary,PASSWORD_LENGTH)
+    candidates = gutenbergPasswordGenerator.generate_password(sample_dictionary,PASSWORD_LENGTH)
     valid = True
     for candidate in candidates:
         digits = [x for x in candidate if x.isdigit()]
@@ -74,7 +74,7 @@ def test_generate_password_two_digits(sample_dictionary):
 
 def test_generate_password_password_length(sample_dictionary):
     PASSWORD_LENGTH = 20
-    candidates = GetNovelText.generate_password(sample_dictionary,PASSWORD_LENGTH)
+    candidates = gutenbergPasswordGenerator.generate_password(sample_dictionary,PASSWORD_LENGTH)
     valid = True
     for candidate in candidates:
         if len(candidate) < PASSWORD_LENGTH:
@@ -83,7 +83,7 @@ def test_generate_password_password_length(sample_dictionary):
 
 def test_generate_password_contains_special_character(sample_dictionary):
     PASSWORD_LENGTH = 20
-    candidates = GetNovelText.generate_password(sample_dictionary,PASSWORD_LENGTH)
+    candidates = gutenbergPasswordGenerator.generate_password(sample_dictionary,PASSWORD_LENGTH)
     valid = True
     for candidate in candidates:
         valid = False
